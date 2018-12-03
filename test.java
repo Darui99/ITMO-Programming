327706f9ed513c70b735e1270a2cd26aac12f8f6
Darui99 <45574484+Darui99@users.noreply.github.com>
2018-12-03 23:12:41 +0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.io.PrintWriter;
+
+public class AplusB {
+	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in);
+      	PrintWriter out = new PrintWriter(System.out);
+
+		int a = in.nextInt();
+		int b = in.nextInt();
+
+		System.out.println(a + b);
+		}
+}
