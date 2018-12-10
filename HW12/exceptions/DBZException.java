c33e0e00ef2b276fc3850516959b526f0f15cd61
Egor Kurbatov <45574484+Darui99@users.noreply.github.com>
2018-12-10 18:30:56 +0300

    Add files via upload

---
+++ HW12/exceptions/DBZException.java
@@ -0,0 +1,7 @@
+package expression.exceptions;
+
+public class DBZException extends EvaluateException {
+    public DBZException(String message) {
+        super(message);
+    }
+}
---
+++ HW12/exceptions/EvaluateException.java
@@ -0,0 +1,10 @@
+package expression.exceptions;
+
+public class EvaluateException extends Exception {
+    public EvaluateException() {
+        super();
+    }
+    public EvaluateException(String message) {
+        super(message);
+    }
+}
